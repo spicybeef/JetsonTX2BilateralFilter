7cf98aa2c61c95ee35052cd540b71d93db15014b
Michel Kakulphimp <michelk@gmail.com>
2018-10-28 23:40:09 +0000

    Adding CV bilateral filter call

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,40 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "main.h"
 
 int main( int argc, char** argv )
 {
     cv::String inputImagePath("input.png");
-    cv::Mat image;
+    cv::Mat inputImage;
+    cv::Mat outputImage;
 
     // If we've passed in an image, use that one instead
     if( argc > 1)
     {
         inputImagePath = argv[1];
     }
-
+    
     // Parse in the image
-    image = cv::imread(inputImagePath, cv::IMREAD_GRAYSCALE);
+    inputImage = cv::imread(inputImagePath, cv::IMREAD_GRAYSCALE);
 
     // Check if that worked
-    if (image.empty())
+    if (inputImage.empty())
     {
         std::cout << "Error while attempting to open image" << std::endl;
         std::cin.get();
         return -1;
     }
 
+    // Try running the CV bilateral filter on it
+    cv::bilateralFilter(inputImage,outputImage,20,75,75);
+
     // Create window for display
-    cv::namedWindow("Output", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Output", 1024, 768);
+    cv::namedWindow("Output", cv::WINDOW_AUTOSIZE);
     // Show the image inside of it
-    cv::imshow("Output", image);
+    cv::imshow("Output", outputImage);
     // Wait for a keystroke in the window
     cv::waitKey(0);
 
