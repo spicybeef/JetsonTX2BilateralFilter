88014752dc3d1b151e7cf15d8fe93c62f52ae4ad
Michel Kakulphimp <michelk@gmail.com>
2018-12-04 08:22:55 +0000

    Cleanup

--- src/bilateral_cpu.cpp
+++ src/bilateral_cpu.cpp
@@ -1,11 +1,100 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <math.h>
+#include <arm_neon.h>
 
+// Duplication of exp implementation for assembly analysis. Wasn't able to look
+// at the disassembly of built-in expf function because it made a call into a
+// library I couldn't disassemble easily. This is taken directly out of the GNU
+// C Library (glibc). For some reason this is faster than using the builtin expf
+// function (???).
 extern "C"
 {
-    // This is defined in main.cpp
-    float expf_michel (float x);
+    static inline double_t roundtoint (double_t x)
+    {
+        return vget_lane_f64 (vrndn_f64 (vld1_f64 (&x)), 0);
+    }
+    static inline uint64_t converttoint (double_t x)
+    {
+        return vcvtnd_s64_f64 (x);
+    }
+    static inline double asdouble (uint64_t i)
+    {
+        union
+        {
+            uint64_t i;
+            double f;
+        }
+        u = {i};
+        return u.f;
+    }
+    #define EXP2F_TABLE_BITS 5
+    #define EXP2F_POLY_ORDER 3
+    extern const struct exp2f_data_michel
+    {
+      uint64_t tab[1 << EXP2F_TABLE_BITS];
+      double shift_scaled;
+      double poly[EXP2F_POLY_ORDER];
+      double shift;
+      double invln2_scaled;
+      double poly_scaled[EXP2F_POLY_ORDER];
+    } __exp2f_data_michel;
+    #define N (1 << EXP2F_TABLE_BITS)
+    const struct exp2f_data_michel __exp2f_data_michel =
+    {
+        /* tab[i] = uint(2^(i/N)) - (i << 52-BITS)
+         used for computing 2^(k/N) for an int |k| < 150 N as
+         double(tab[k%N] + (k << 52-BITS)) */
+        .tab =
+        {
+            0x3ff0000000000000, 0x3fefd9b0d3158574, 0x3fefb5586cf9890f, 0x3fef9301d0125b51,
+            0x3fef72b83c7d517b, 0x3fef54873168b9aa, 0x3fef387a6e756238, 0x3fef1e9df51fdee1,
+            0x3fef06fe0a31b715, 0x3feef1a7373aa9cb, 0x3feedea64c123422, 0x3feece086061892d,
+            0x3feebfdad5362a27, 0x3feeb42b569d4f82, 0x3feeab07dd485429, 0x3feea47eb03a5585,
+            0x3feea09e667f3bcd, 0x3fee9f75e8ec5f74, 0x3feea11473eb0187, 0x3feea589994cce13,
+            0x3feeace5422aa0db, 0x3feeb737b0cdc5e5, 0x3feec49182a3f090, 0x3feed503b23e255d,
+            0x3feee89f995ad3ad, 0x3feeff76f2fb5e47, 0x3fef199bdd85529c, 0x3fef3720dcef9069,
+            0x3fef5818dcfba487, 0x3fef7c97337b9b5f, 0x3fefa4afa2a490da, 0x3fefd0765b6e4540,
+        },
+        .shift_scaled = 0x1.8p+52 / N,
+        .poly = { 0x1.c6af84b912394p-5, 0x1.ebfce50fac4f3p-3, 0x1.62e42ff0c52d6p-1 },
+        .shift = 0x1.8p+52,
+        .invln2_scaled = 0x1.71547652b82fep+0 * N,
+        .poly_scaled = { 0x1.c6af84b912394p-5/N/N/N, 0x1.ebfce50fac4f3p-3/N/N, 0x1.62e42ff0c52d6p-1/N },
+    };
+    #define InvLn2N __exp2f_data_michel.invln2_scaled
+    #define T __exp2f_data_michel.tab
+    #define C __exp2f_data_michel.poly_scaled
+
+    float expf_michel (float x)
+    {
+        uint64_t ki, t;
+        /* double_t for better performance on targets with FLT_EVAL_METHOD==2.  */
+        double_t kd, xd, z, r, r2, y, s;
+
+        xd = (double_t) x;
+
+        /* x*N/Ln2 = k + r with r in [-1/2, 1/2] and int k.  */
+        z = InvLn2N * xd;
+
+        /* Round and convert z to int, the result is in [-150*N, 128*N] and
+         ideally ties-to-even rule is used, otherwise the magnitude of r
+         can be bigger which gives larger approximation error.  */
+        kd = roundtoint(z);
+        ki = converttoint(z);
+        r = z - kd;
+
+        /* exp(x) = 2^(k/N) * 2^(r/N) ~= s * (C0*r^3 + C1*r^2 + C2*r + 1) */
+        t = T[ki % N];
+        t += ki << (52 - EXP2F_TABLE_BITS);
+        s = asdouble(t);
+        z = C[0] * r + C[1];
+        r2 = r * r;
+        y = C[2] * r + 1;
+        y = z * r2 + y;
+        y = y * s;
+        return (float) y;
+    }
 }
 
 /**
